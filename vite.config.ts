6e1563fa398768283be9e9aae1852c0c53329ed1
DevSupAi <contact@devsupai.fr>
2026-08-05 09:41:43 +0200

    seo: automate sitemap and robots.txt generation using vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://devsupai.fr',
+      readable: true,
+      dynamicRoutes: [], // If you add more client routes, append them here
+    }),
+  ],
 })
